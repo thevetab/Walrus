63980e2313750973d6226f4951d90ec9627660fe
Hailey Murto and Matt Phillips <student@epicodus.com>
2016-02-01 20:55:37 -0800

    Makes whole website.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("this is a header.");
+  });
+
+  $("h2").click(function() {
+    alert("this is a second header.")
+  });
+
+  $("p").dblclick(function() {
+    alert("this is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("this is an image.");
+  });
+
+
+});
