630157c368a2447c8ea4b7f525976057bf362a38
matt <thevetab@gmail.com>
2016-02-07 18:42:51 -0800

    practice jquery basics on walrus page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
   $("h1").click(function() {
     alert("this is a header.");
   });
@@ -7,13 +7,16 @@ $(document).ready(function() {
     alert("this is a second header.")
   });
 
-  $("p").dblclick(function() {
-    alert("this is a paragraph.");
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 
   $("img").click(function() {
     alert("this is an image.");
+    $("img").fadeIn();
   });
 
-
 });
+
+
